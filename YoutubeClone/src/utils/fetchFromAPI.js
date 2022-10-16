ac3741f918e025498597846301acb213bec433f1
SOHAM SARKAR <98482003+Sohoxic@users.noreply.github.com>
2022-10-16 11:50:14 +0530

    Update fetchFromAPI.js

--- YoutubeClone/src/utils/fetchFromAPI.js
+++ YoutubeClone/src/utils/fetchFromAPI.js
@@ -1,15 +1,19 @@
 import axios from 'axios';
 
-export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
+  url: BASE_URL,
   params: {
-    maxResults: 50,
+    relatedToVideoId: '7ghhRHRP6t4',
+    part: 'id,snippet',
+    type: 'video',
+    maxResults: '50'
   },
   headers: {
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
-  },
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+  }
 };
 
 export const fetchFromAPI = async (url) => {
